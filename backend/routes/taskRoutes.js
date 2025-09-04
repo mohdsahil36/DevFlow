d343988a9d078b81da2ffab70c2b0de4d03837cf
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2025-09-04 23:34:51 +0530

    task specific data fetch routes and icon

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -4,12 +4,15 @@ import {
   updateTasks,
   addNewTask,
   deleteTask,
+  getTaskById,
 } from "../services/tasks.js";
 
 const router = express.Router();
 
 // Route to get all tasks
 router.get("/kanban", getAllTasks);
+//Route to fetch id specific data
+router.get("/kanban/:id", getTaskById);
 // Route to update task status
 router.put("/tasks/:id/status", updateTasks);
 //Route to add new task to database
