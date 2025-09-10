ba9dc4643c929e471794f61ded7e2496d9dd301d
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2025-09-10 22:48:24 +0530

    Delete functionality completed

--- frontend/app/components/DeleteConfirmation.tsx
+++ frontend/app/components/DeleteConfirmation.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -8,10 +10,19 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Trash2 } from "lucide-react";
+import { useState } from "react";
 
-export default function DeleteConfirmation() {
+export default function DeleteConfirmation(props: {
+  taskId: string;
+  onTaskDelete: (id: string) => void;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleDelete = () => {
+    props.onTaskDelete(props.taskId);
+    setIsOpen(false);
+  };
   return (
-    <Dialog>
+    <Dialog onOpenChange={setIsOpen} open={isOpen}>
       <DialogTrigger>
         <Trash2 className="size-4 cursor-pointer hover:text-red-400" />
       </DialogTrigger>
@@ -24,8 +35,14 @@ export default function DeleteConfirmation() {
           </DialogDescription>
         </DialogHeader>
         <div className="flex justify-end gap-4 mt-4">
-          <Button className="cursor-pointer">Delete</Button>
-          <Button variant={"secondary"} className="cursor-pointer">
+          <Button className="cursor-pointer" onClick={() => handleDelete()}>
+            Delete
+          </Button>
+          <Button
+            variant={"secondary"}
+            className="cursor-pointer"
+            onClick={() => setIsOpen(false)}
+          >
             Cancel
           </Button>
         </div>
