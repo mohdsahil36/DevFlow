3c3753d92fee4bc5887d01f21014fa831f32567c
Mohd Sahil Siddiqui <mohdsahilsiddiqui36@gmail.com>
2025-10-22 23:26:12 +0530

    basic setup

---
+++ backend/models/connection_modal.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Database connection
+const connectionURL = process.env.MONGO_DB_CONNECTION_URL;
+
+if (!connectionURL) {
+  console.error("MONGO_DB_CONNECTION_URL is not defined in .env file");
+  process.exit(1);
+}
+
+
+mongoose
+  .connect(connectionURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to MongoDB Database");
+  })
+  .catch((err) => {
+    console.error("Error connecting to Database:", err);
+    process.exit(1);
+  });
+
+const TaskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  due_date: String,
+  priority: String,
+});
+
+const TaskModel = mongoose.model("Tasks", TaskSchema, "tasks");
+
+export default TaskModel;
