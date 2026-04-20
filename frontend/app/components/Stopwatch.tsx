9ae3544ed4ce98c8399d9888c3551d23b6da7920
Mohd Sahil Siddiqui <mohdsahilsiddiqui36@gmail.com>
2026-04-21 00:00:02 +0530

    pomodoro time formatting , function left for stopwatch

--- frontend/app/components/Stopwatch.tsx
+++ frontend/app/components/Stopwatch.tsx
@@ -5,10 +5,27 @@ import { Button } from "@/components/ui/button";
 
 export default function StopWatch() {
   const [time, setTime] = useState(0);
-  const currentTimeRef = useRef<number>(0);
+  const stopwatchRef = useRef<number>(0); //here we will store the values of the time in ms
+  const intervalRef = useRef(0);
+
+  function formatTime() {
+    const ms = Math.floor((time % 1000) / 10); // hundredths of a second
+    const sec = Math.floor((time / 1000) % 60); // seconds (0–59)
+    const min = Math.floor((time / (1000 * 60)) % 60); // minutes (0–59)
+    const hr = Math.floor(time / (1000 * 60 * 60)); // hours
+
+    return `${pad(hr)}:${pad(min)}:${pad(sec)}.${pad(ms)}`;
+  }
+
+  function pad(num: number) {
+    return num.toString().padStart(2, "0");
+  }
 
   function startStopWatch() {
-    currentTimeRef.current = Date.now();
+    stopwatchRef.current = new Date().getTime();
+    intervalRef.current = setInterval(() => {
+      setTime(new Date().getTime() - stopwatchRef.current);
+    }, 10);
   }
 
   function pauseStopWatch() {}
@@ -17,7 +34,7 @@ export default function StopWatch() {
 
   return (
     <div className="max-w-3xl mx-auto bg-red-500">
-      <h1 className="text-center">{time}</h1>
+      <h1 className="text-center">{formatTime()}</h1>
       <div className="text-center">
         <Button onClick={startStopWatch}>Start</Button>
         <Button onClick={pauseStopWatch}>Pause</Button>
